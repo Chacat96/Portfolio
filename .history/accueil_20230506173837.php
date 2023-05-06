2ccd37ae49dd7de4f4d4474add79222146f658af
= <c.catherin@outlook.fr>
2023-05-06 18:19:37 +0200

    caroussel

--- .history/accueil_20230506173837.php
+++ .history/accueil_20230506173837.php
@@ -11,11 +11,11 @@ require_once "core/entity/header.php";
         <section class="slideshow">
             <div class="content">
                 <div class="content-carrousel">
-                    <figure class="shadow"><a href="competence.php"><img src="img/skill.jpg" alt=""></a><p class="legende">Mes compétences</p></figure>
-                    <figure class="shadow"><a href="projet.php"><img src="img/laptop-5673901_1920.jpg"></a><p class="legende">Mes projets</p></figure>
-                    <figure class="shadow"><a href="contact.php"><img src="img/image formulaire.png" alt=""></a><p class="legende">Contact</p></figure>
-                    <figure class="shadow"><a href="cv.php"><img src="img/cv.png" alt="" ></a><p class="legende">CV</p></figure>
-                    <figure class="shadow"><a href="description.php"><img src="img/moianime.jpg" alt=""></a><p class="legende">Ma description</p></figure>
+                    <figure class="shadow"><a href="competence.php"><img src="img/skill.jpg" alt=""></a></figure><p class="legende">Mes compétences</p>
+                    <figure class="shadow"><a href="projet.php"><img src="img/laptop-5673901_1920.jpg"></a></figure><p class="legende">Mes projets</p>
+                    <figure class="shadow"><a href="contact.php"><img src="img/image formulaire.png" alt=""></a></figure><p class="legende">Contact</p>
+                    <figure class="shadow"><a href="cv.php"><img src="img/cv.png" alt="" ></a></figure><p class="legende">CV</p>
+                    <figure class="shadow"><a href="description.php"><img src="img/moianime.jpg" alt=""></a></figure><p class="legende">Ma description</p>
                 </div>
                 <div class="content-carrousel-mobile">
                     <figure class="shadow"><a href="competence.php"><img src="img/computer-1836330_1920.png" alt=""><p class="legende">Mes compétences</p></a></figure>
